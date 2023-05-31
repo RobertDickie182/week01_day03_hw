b4ebafe8eea0c31a6219cd92d361e2da6af3bc16
RobertDickie182 <robertdanieldickie@gmail.com>
2023-05-31 23:24:25 +0100

    First commit hw

---
+++ src/python_functions_practice.py
@@ -0,0 +1,41 @@
+def return_10():
+ return 10
+
+def add(first_num, second_num):
+ return first_num + second_num
+
+def subtract(first_num, second_num):
+ return first_num - second_num
+
+def multiply(first_num, second_num):
+ return first_num * second_num
+
+def divide(first_num, second_num):
+ return first_num / second_num
+
+def length_of_string(string):
+ return len(string)
+
+def join_string(first_string, second_string):
+ return first_string + second_string
+
+def add_string_as_number(first_string, second_string):
+ return int(first_string) + int(second_string)
+
+def number_to_full_month_name(number):
+ import calendar
+ month = number
+ return calendar.month_name[number]
+
+def number_to_short_month_name(number):
+ import calendar
+ month = number
+ return calendar.month_abbr[number]
+
+
+
+
+
+ 
+ 
+ 
